8a501338821788bcc9d43f3ef0c15efe70adf4a4
Mohamed Haaris <haaristhere@gmail.com>
2024-03-27 20:40:32 +0530

    Add files via upload
    Day 9

---
+++ Backend/Day-9/boathouse-backend/src/main/java/com/boat/boathouse/repository/BookingRepository.java
@@ -0,0 +1,17 @@
+package com.boat.boathouse.repository;
+// package com.haaris.boathouse.repository;
+
+// import com.haaris.boathouse.model.Booking;
+// import com.haaris.boathouse.model.Passenger;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.stereotype.Repository;
+
+// import java.util.List;
+
+// @Repository
+// public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+//     @Query("SELECT b.bookingId, p.name, p.contactNumber, b.boat.boatName, b.boat.boatClass, b.checkOut, b.boat.fare FROM Booking b JOIN b.passengers p")
+//     List<Object[]> getBookingDetails();
+// }
